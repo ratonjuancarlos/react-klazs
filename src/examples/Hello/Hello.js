6f9a9b99f7190661053a2336c60c9400e65f9286
Pablo H <ratonjuancarlos@users.noreply.github.com>
2018-04-05 14:24:01 -0300

    update with comment to clarify

--- src/examples/Hello/Hello.js
+++ src/examples/Hello/Hello.js
@@ -1,13 +1,18 @@
 import React from "react";
 import klazs from "../../lib";
+import styles from './styles.scss'
 
-const styles = {
-  textPrimary: "textPrimary"
-};
+//const styles = {
+// textPrimary: "textPrimary_sdkhgfjkhsdgfsdf"
+//};
 
 const Hello = props => {
   const { children } = props;
   const myKlazs = klazs(styles, props);
+  
+  // myKlazs = {
+  //  textPrimary: "textPrimary_sdkhgfjkhsdgfsdf textImportant" 
+  //}
 
   return <h2 className={myKlazs.textPrimary}> {children}</h2>;
 };
